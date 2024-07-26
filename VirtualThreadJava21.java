d342479d7f8a42638084a7084605110ea02b08a2
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-07-27 03:19:53 +0530

    feat: Added Thread.sleep(500), executor.awaitTermination() and virtualThread.join() ->(Java 21)

--- VirtualThreadJava21.java
+++ VirtualThreadJava21.java
@@ -1,11 +1,20 @@
 import java.util.concurrent.Executors;
 public class VirtualThreadJava21 {
-    public static void main (String[]args){
-        Runnable runnable = () -> System.out.println("Inside Runnable:"+Thread.currentThread().getName());
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = () -> {
+            try {
+                Thread.sleep(500);
+                System.out.println("Inside Runnable: " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        };
         Thread.startVirtualThread(runnable);
         Thread virtualThread = Thread.ofVirtual().start(runnable);
         var executor = Executors.newVirtualThreadPerTaskExecutor();
         executor.submit(runnable);
         executor.shutdown();
+        executor.awaitTermination(1, java.util.concurrent.TimeUnit.MINUTES);
+        virtualThread.join();
     }
 }
